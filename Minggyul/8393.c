70ab5088d64681b2de2992d85d0db6a58f57d850
Minggyul <153511669+Minggyul@users.noreply.github.com>
2023-12-12 19:45:52 +0900

    Create 8393.c

---
+++ Minggyul/8393.c
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+int main(){
+    FASTIO;
+    
+    int n; cin >> n;
+    cout << (n * (n + 1)) / 2;
+}
