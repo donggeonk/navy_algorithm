6fedaf1431fb7c3e51be9066899157b852dd7991
Donggeon Kim (Steve) <76398774+donggeonk@users.noreply.github.com>
2023-12-13 20:41:26 +0900

    Create 8393.py

---
+++ week_01/Donggeon/8393.py
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int ans = (1 + n) * n / 2;
+        System.out.println(ans);
+    }
+}
