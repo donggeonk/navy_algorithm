85afc160310d08c61814e72e00c967f5931254b6
Donggeon Kim (Steve) <76398774+donggeonk@users.noreply.github.com>
2023-12-24 15:54:06 +0900

    Update 1912.java

--- week_01/Donggeon/1912.java
+++ week_01/Donggeon/1912.java
@@ -1,16 +1,29 @@
-// Brute force search
 import java.util.Scanner;
 
-public class Main {
+/*
+O(n)
+- in a loop, add next int to the sum
+- drop the sum when it goes below 0
+- compare the sum with the max and update it
+
+(Brute force -> O(n^2))
+*/
+
+public class Main{
     public static void main(String args[]) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
-        int max = 0;
+        int sum = 0;
+        int max = -1000;
+        
         for(int i = 0; i < n; i++){
             int a = input.nextInt();
-            if (max + a > max) {
-                max = max + a;
+            sum = (sum <= 0) ? a : sum+a;
+            if(sum > max){
+                max = sum;
             }
         }
+        
+        System.out.println(max);
     }
 }
