e5b2bb59661f0360cca2fc9f1a6d91ff52c71d50
Minggyul <153511669+Minggyul@users.noreply.github.com>
2024-01-03 18:40:27 +0900

    Create 11726.c

---
+++ week_04/Minggyul/11726.c
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define FASTIO ios::sync_with_stdio(0), cin.tie(0), cout.tie(0)
+using namespace std;
+
+#define ll long long
+const int MAX = 1005;
+ll DP[MAX];
+
+int main(){
+    FASTIO;
+
+    int n; cin >> n;
+    DP[1] = 1, DP[2] = 2;
+    for (int i = 3; i <= n; i++) DP[i] = (DP[i - 2] + DP[i - 1]) % 10007;
+    cout << DP[n];
+    return 0;
+}
